8469721a836f2f348783547d3bde306119b5e1ae
Krest0s <47479838+Krest0s@users.noreply.github.com>
2019-04-25 22:39:04 +0300

    увеличил шрифт

--- JusticeGame.py
+++ JusticeGame.py
@@ -3,17 +3,17 @@ import random
 
 massive=[]
 category = ['Страна','Столица','Правитель']
-
+n=8
 def openfile():
     f=open('massive.txt')
     for i in range(3):
         a = []
-        for j in range(5):
+        for j in range(n):
             a.insert(j,f.readline())
         massive.insert(i, a)
     f.close()
     for i in range(3):
-        for j in range(5):
+        for j in range(n):
             massive[i][j] = massive[i][j].replace('\n', '')
 
 def country():
@@ -27,6 +27,7 @@ def country():
         label_var.set('Случайная страна - ' + ra)
         label.update()
 
+
 def capital():
     if massive[1]==[]:
         label_var.set('Игра окончена!')
@@ -38,37 +39,42 @@ def capital():
         label_var.set('Случайная столица - ' + ra)
         label.update()
 
+
 def ruler():
     if massive[2]==[]:
-        #text.delete(1.0, END)
         label_var.set('Игра окончена!')
     else:
         i=random.randint(0,len(massive[2])-1)
         ra=massive[2].pop(i)
         del massive[0][i]
         del massive[1][i]
-        label_var.set('Случайный правитель - ' + ra)
-        label.update
+        label_var.set('Случайный петушара - ' + ra)
+        label.update()
 
 openfile()
 
 root = Tk()
+#root.attributes('-fullscreen', True)
 root.title("Justice Game")
 root.iconbitmap('icon.ico')
-root.geometry("640x640")
+w, h = root.winfo_screenwidth(), root.winfo_screenheight()
+#use the next line if you also want to get rid of the titlebar
+root.geometry("%dx%d+0+0" % (w, h))
+#root.geometry("fullscreen")
 w=10
 h=1
 s='top'
 x=50
+f='Arial 20'
 
 label_var = StringVar()
-label = Label(root, font='40', bg="white", width=100, height=13, textvariable=label_var)
+label = Label(root, font='Calibri 29', bg="white", width=100, height=13, textvariable=label_var)
 label.pack()
 
-btn1 = Button(text=category[0], background="white", foreground="black", width=w, height=h, font="16", command=country,padx=x)
+btn1 = Button(text=category[0], background="white", foreground="black", width=w, height=h, font=f, command=country,padx=x)
 btn1.pack(side=s)
-btn2 = Button(text=category[1], background="white", foreground="black", width=w, height=h, font="16", command=capital,padx=x)
+btn2 = Button(text=category[1], background="white", foreground="black", width=w, height=h, font=f, command=capital,padx=x)
 btn2.pack(side=s)
-btn3 = Button(text=category[2], background="white", foreground="black", width=w, height=h, font="16", command=ruler,padx=x)
+btn3 = Button(text=category[2], background="white", foreground="black", width=w, height=h, font=f, command=ruler,padx=x)
 btn3.pack(side=s)
 root.mainloop()
